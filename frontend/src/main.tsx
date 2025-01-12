5b2260fe7189cea929b7750a528aeab6e40ed1a5
Harsh kumar <95240459+Harshksaw@users.noreply.github.com>
2025-01-12 08:27:21 -0800

    Update index.html title and remove file path comment in main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,4 +1,3 @@
-// filepath: /Users/harshsaw/Documents/GitHub/Mobile-ordering/frontend/src/main.tsx
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from 'react-router-dom';
 import "./index.css";
