698f9cde0710006341da13efb045862ce1e8bbe3
Gourish Markan <142171408+GourishMarkan@users.noreply.github.com>
2025-01-12 12:45:01 +0530

    Refactor App component by commenting out unused imports and code

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,24 @@
-import { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
-import { QRCodeSVG } from 'qrcode.react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import { useState } from 'react'
+// import { useNavigate } from 'react-router-dom'
+// import { QRCodeSVG } from 'qrcode.react'
+
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-  const navigate = useNavigate()
+  // const [count, setCount] = useState(0)
+  // const navigate = useNavigate()
 
-  const qrCodeValue = `${window.location.origin}/orders`;
+  // const qrCodeValue = `${window.location.origin}/orders`;
 
   return (
     <>
-      <div cls="h-full w-full bg-green-800 text-red-500 flex items-center justify-center">
+      <div className="h-full w-full bg-green-800 text-red-500 flex items-center justify-center">
         he
         {/* <h1>Scan the QR Code to go to Orders</h1>
         <QRCodeSVG value={qrCodeValue} size={256} /> */}
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
